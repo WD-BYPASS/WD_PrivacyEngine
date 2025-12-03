46c51c79eaddb82d4c9243acdcb15859904e4867
Scarlett/Eight <bennettbashford@gmail.com>
2025-12-03 18:58:56 +0000

    eee

--- Password security tester.py
+++ Password security tester.py
@@ -2,8 +2,14 @@ import itertools
 import threading
 
 charlist = ["A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z", "]", "[", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "|", ":", ";", "\"", "'", "<", ">", ",", ".", "?", "/", "~", "`", " ", "\\", " ", "¡", "¢", "£", "¤", "¥", "¦", "§", "¨", "©", "ª", "«", "¬", "®", "¯", "°", "±", "²", "³", "´", "µ", "¶", "·", "¸", "¹", "º", "»", "¼", "½", "¾", "¿"]
-password = ""
+password = input("Enter the password to crack: ")
 maxlength = int(input("Enter maximum password length: "))
+
+def check_password(pw):
+    # Placeholder for actual password checking logic
+    # For demonstration, let's assume the correct password is "Ab1!"
+    correct_password = password
+    return pw == correct_password
 
 def trypassword():
     global password
@@ -11,11 +17,8 @@ def trypassword():
         for attempt in itertools.product(charlist, repeat=length):
             password = ''.join(attempt)
             print(f"Trying password: {password}")
-            # Here you would add the code to test the password against the target system
-            # For example, attempting to log in or decrypt a file
-            # If the password is correct, you can break out of the loops
-            # if test_password(password):
-            #     print(f"Password found: {password}")
-            #     return
+            if check_password(password):
+                print(f"Password found: {password}")
+                return
 
 trypassword()
